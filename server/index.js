dc60663bc5c552d81662686cd3f0950396bbf66f
Christopher MCAdams <mca.christopher@gmail.com>
2019-07-17 00:54:56 -0400

    PWA poc WIP

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const path  = require('path');
 const express = require('express');
+const publicPath = path.resolve(__dirname, '../dist/');
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, 'views')));
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(`${publicPath}/index.html`);
 });
 
 app.listen(8080, () => console.log('ready'));
---
+++ workbox-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globDirectory: './dist',
+  globPatterns: ['\*\*/\*.{html,js,css}'],
+  swDest: './dist/sw.js'
+}
