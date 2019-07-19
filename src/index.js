58f163dfd90bb1855fd99fed5a70b1fb0596fe01
Christopher MCAdams <mca.christopher@gmail.com>
2019-07-19 11:01:49 -0400

    adding timeout to mock server request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import app from './app/index.js';
 
 window.fetch('/connect?type=patch')
-  .then((res) => res.json())
+  .then((res) => {
+    // simulate time to grab data
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(res.json());
+      }, 2000);
+    });
+  })
   .then(({context = 'complete'} = json) => {
-    app.render(context);
+    console.log(`rendering ${context}`);
+    return app.render(context);
   });
 
 window.addEventListener('load', () => {
+  app.render();
   navigator.serviceWorker.register('./sw.js');
 });
 
--- src/patchConnect/index.js
+++ src/patchConnect/index.js
@@ -1,18 +1,8 @@
 import Bluetooth from '../Bluetooth';
-const template = (message = "NOT Connected?") => {
-
-return `
-<div id="connect">${message}</div>
-`
-}
+const template = (message = "NOT Connected?") => ` <div id="connect">${message}</div> `;
 
 const patchConnect = {
-  connect() {
-    Bluetooth.add()
-  }
-
   render() {
-    this.connect();
     return template();
   }
 };
